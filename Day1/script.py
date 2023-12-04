b646555ab5dc21a5464391d3301b9e4a86b208aa
Opris <uif93501@contiwan.com>
2023-12-04 14:49:00 +0200

    solution #2

--- Day1/script.py
+++ Day1/script.py
@@ -1,7 +1,20 @@
 file = open("input.txt", "r")
 final = []
+
 for line in file.readlines():
-    res = [int(i) for i in [*line] if i.isdigit()]
+    data = (
+    line.replace("one", "one1one")
+    .replace("two", "two2two")
+    .replace("three", "three3three")
+    .replace("four", "four4four")
+    .replace("five", "five5five")
+    .replace("six", "six6six")
+    .replace("seven", "seven7seven")
+    .replace("eight", "eight8eight")
+    .replace("nine", "nine9nine")
+    )
+    res = [int(i) for i in data if i.isdigit()]
+    print(res)
     first = [res[0]]
     last = [res[-1]]
     res1 = first + last
